82e1c6fa061f714e4032ab49fed4c66bc7aa3489
DavidMaxwell616 <33132201+DavidMaxwell616@users.noreply.github.com>
2019-07-12 22:40:17 -0700

    Updates

--- js/game.js
+++ js/game.js
@@ -4,38 +4,15 @@ const game = new Phaser.Game(800, 500, Phaser.AUTO, 'game', {
   update,
 });
 
-var jumpdistance = 0;
-var jumpheight = 50;
 var XVelocity = 2.8;
 var YVelocity = -10;
-var gravity = .5;
-var jumping;
-var shortJump = 40;
+var gravity = 0.5;
+var shortJump = 41;
 var longJump = 80;
 
 function create() {
-  //game.stage.backgroundColor = '#001133';
   DrawBackground();
-  for (let i = 0; i < 3; i++) {
-    leftFrog[i] = game.add.sprite(i * 115, 215, 'leftFrog');
-    leftFrog[i].inputEnabled = true;
-    leftFrog[i].events.onInputDown.add(listener, this);
-    leftFrog[i].scale.x = .4;
-    leftFrog[i].scale.y = .4;
-    leftFrog[i].id = i;
-    leftFrog[i].animations.add('jump');
-  }
-
-  for (let i = 4; i < 7; i++) {
-    rightFrog[i - 4] = game.add.image(i * 120 + 85, 210, 'rightFrog');
-    rightFrog[i - 4].scale.x = -.4;
-    rightFrog[i - 4].scale.y = .4;
-    rightFrog[i - 4].inputEnabled = true;
-    rightFrog[i - 4].events.onInputDown.add(listener, this);
-    rightFrog[i - 4].id = i;
-    rightFrog[i - 4].animations.add('jump');
-  }
-
+  InitFrogs();
 }
 
 function DrawBackground() {
@@ -44,8 +21,7 @@ function DrawBackground() {
   background.height = game.height;
   for (let i = -20; i < game.width; i++) {
     let g = game.rnd.integerInRange(1, 100);
-    if (g < 15)
-      grass[i] = game.add.image(i, 80 + g * 2, 'grass');
+    if (g < 15) grass[i] = game.add.image(i, 80 + g * 2, 'grass');
   }
   bank = game.add.image(0, 130, 'bank');
   bank.width = game.width + 20;
@@ -58,28 +34,28 @@ function DrawBackground() {
 
   for (let i = 0; i < 3; i++) {
     cattail[i] = game.add.image(i * 60 + 5, 50, 'cattail');
-    cattail[i].scale.x = .5;
-    cattail[i].scale.y = .5;
+    cattail[i].scale.x = 0.5;
+    cattail[i].scale.y = 0.5;
   }
   for (let i = 1.5; i < 3.5; i++) {
     cattail[i] = game.add.image(i * 60 + 5, 100, 'cattail');
-    cattail[i].scale.x = -.3;
-    cattail[i].scale.y = .3;
+    cattail[i].scale.x = -0.3;
+    cattail[i].scale.y = 0.3;
   }
   for (let i = 6; i < 8; i++) {
     cattail[i] = game.add.image(i * 60 + 5, 90, 'cattail');
-    cattail[i].scale.x = .4;
-    cattail[i].scale.y = .4;
+    cattail[i].scale.x = 0.4;
+    cattail[i].scale.y = 0.4;
   }
   for (let i = 12; i < 14; i++) {
     cattail[i] = game.add.image(i * 60 + 5, 50, 'cattail');
-    cattail[i].scale.x = -.5;
-    cattail[i].scale.y = .5;
+    cattail[i].scale.x = -0.5;
+    cattail[i].scale.y = 0.5;
   }
   for (let i = 12.5; i < 14.5; i++) {
     cattail[i] = game.add.image(i * 60 + 5, 90, 'cattail');
-    cattail[i].scale.x = -.4;
-    cattail[i].scale.y = .4;
+    cattail[i].scale.x = -0.4;
+    cattail[i].scale.y = 0.4;
   }
 
   overlay = game.add.image(0, 0, 'overlay');
@@ -88,35 +64,86 @@ function DrawBackground() {
 
   reset = game.add.image(20, 420, 'reset');
   reset.inputEnabled = true;
-  // reset.events.onInputDown.add(reset, this);
+  reset.events.onInputDown.add(listener, this);
+}
+
+function DestroyFrogs() {
+  for (let i = 0; i < 7; i++) {
+    rockTaken[i] = true;
+  }
+  rockTaken[3] = false;
+  for (let i = 0; i < 3; i++) {
+    leftFrog[i].destroy();
+  }
+
+  for (let i = 4; i < 7; i++) {
+    rightFrog[i - 4].destroy();
+  }
+}
+
+function InitFrogs() {
+  for (let i = 0; i < 3; i++) {
+    leftFrog[i] = game.add.sprite(i * 115, 215, 'leftFrog');
+    leftFrog[i].inputEnabled = true;
+    leftFrog[i].events.onInputDown.add(listener, this);
+    leftFrog[i].scale.x = 0.4;
+    leftFrog[i].scale.y = 0.4;
+    leftFrog[i].id = i;
+    leftFrog[i].jumping = false;
+    leftFrog[i].jumpdistance = 0;
+    leftFrog[i].animations.add('jump');
+  }
+
+  for (let i = 4; i < 7; i++) {
+    rightFrog[i - 4] = null;
+    rightFrog[i - 4] = game.add.image(i * 120 + 85, 210, 'rightFrog');
+    rightFrog[i - 4].scale.x = -0.4;
+    rightFrog[i - 4].scale.y = 0.4;
+    rightFrog[i - 4].inputEnabled = true;
+    rightFrog[i - 4].events.onInputDown.add(listener, this);
+    rightFrog[i - 4].id = i;
+    rightFrog[i - 4].jumping = false;
+    rightFrog[i - 4].jumpdistance = 0;
+    rightFrog[i - 4].animations.add('jump');
+  }
 }
 
 function reset(sprite) {
-
+  InitFrogs();
 }
 
 function listener(sprite) {
-  if (sprite.key === "leftFrog") {
-    sprite.jumping = true;
-    sprite.animations.play('jump', 10, false);
-    // if (sprite.id < 7 && !rockTaken[sprite.id + 1]) {
-    //   sprite.jumping = true;
-    //   sprite.doubleJump = false;
-    //   sprite.animations.play('jump', 10, false);
-    // } else if (sprite.id < 6 && !rockTaken[sprite.id + 2]) {
-    //   sprite.jumping = true;
-    //   sprite.doubleJump = true;
-    //   sprite.animations.play('jump', 10, false);
-    // }
-  } else if (sprite.key == "rightFrog") {
-    console.log(sprite.id);
+  if (sprite.key === 'leftFrog') {
+    if (sprite.id < 7 && !rockTaken[sprite.id + 1]) {
+      sprite.jumping = true;
+      sprite.doubleJump = false;
+      sprite.animations.play('jump', 10, false);
+      rockTaken[sprite.id + 1] = true;
+      rockTaken[sprite.id] = false;
+    } else if (sprite.id < 6 && !rockTaken[sprite.id + 2]) {
+      sprite.jumping = true;
+      sprite.doubleJump = true;
+      sprite.animations.play('jump', 10, false);
+      rockTaken[sprite.id + 2] = true;
+      rockTaken[sprite.id] = false;
+    }
+  } else if (sprite.key == 'rightFrog') {
     if (sprite.id > 0 && !rockTaken[sprite.id - 1]) {
       sprite.jumping = true;
       sprite.doubleJump = false;
+      sprite.animations.play('jump', 10, false);
+      rockTaken[sprite.id - 1] = true;
+      rockTaken[sprite.id] = false;
     } else if (sprite.id > 1 && !rockTaken[sprite.id - 2]) {
       sprite.jumping = true;
       sprite.doubleJump = true;
+      sprite.animations.play('jump', 10, false);
+      rockTaken[sprite.id - 2] = true;
+      rockTaken[sprite.id] = false;
     }
+  } else if (sprite.key == 'reset') {
+    DestroyFrogs();
+    InitFrogs();
   }
 }
 
@@ -124,13 +151,28 @@ function update() {
   for (let i = 0; i < 3; i++) {
     if (leftFrog[i].jumping) {
       var sprite = leftFrog[i];
-      if (jumpdistance < shortJump) {
-        sprite.x += XVelocity;
+      if (sprite.jumpdistance < shortJump) {
+        if (sprite.doubleJump) sprite.x += XVelocity * 2;
+        else sprite.x += XVelocity;
         sprite.y += YVelocity;
         YVelocity += gravity;
-        jumpdistance++;
+        sprite.jumpdistance++;
       } else {
-        jumpdistance = 0;
+        sprite.jumpdistance = 0;
+        sprite.jumping = false;
+        YVelocity = -10;
+        sprite.frame = 0;
+      }
+    } else if (rightFrog[i].jumping) {
+      var sprite = rightFrog[i];
+      if (sprite.jumpdistance < shortJump) {
+        if (sprite.doubleJump) sprite.x -= XVelocity * 2;
+        else sprite.x -= XVelocity;
+        sprite.y += YVelocity;
+        YVelocity += gravity;
+        sprite.jumpdistance++;
+      } else {
+        sprite.jumpdistance = 0;
         sprite.jumping = false;
         YVelocity = -10;
         sprite.frame = 0;
